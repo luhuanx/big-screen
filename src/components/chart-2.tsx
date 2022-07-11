0992dd6e54eaddc004117dc530dd07925ace3fcb
Mimanchi <1640247539@qq.com>
2022-07-11 10:55:04 +0800

    完成chart2

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import * as echarts from "echarts";
-import { baseEchartOptions } from "../shared/base-echart-options";
-import { px } from "../shared/px";
+
 import { createEchartOptions } from "../shared/create-echarts-options";
 
 export const Chart2 = () => {
@@ -14,12 +13,20 @@ export const Chart2 = () => {
       xAxis: {
         type: "value",
         boundaryGap: [0, 0.01],
-        splitLine: { show: false },
-        axisLabel: { show: false },
+        splitLine: {
+          show: false,
+        },
+
+        axisLabel: {
+          show: false,
+        },
       },
 
       yAxis: {
-        axisTick: { show: false },
+        axisTick: {
+          show: false,
+        },
+
         type: "category",
         data: [
           "城关区公安局",
@@ -44,12 +51,42 @@ export const Chart2 = () => {
           name: "破案排名1",
           type: "bar",
           data: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+          itemStyle: {
+            normal: {
+              color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                {
+                  offset: 0,
+                  color: "#2034f9",
+                },
+
+                {
+                  offset: 1,
+                  color: "#04a1ff",
+                },
+              ]),
+            },
+          },
         },
 
         {
           name: "破案排名2",
           type: "bar",
           data: [2, 3, 4, 5, 6, 7, 8, 9, 10],
+          itemStyle: {
+            normal: {
+              color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                {
+                  offset: 0,
+                  color: "#b92ae8",
+                },
+
+                {
+                  offset: 1,
+                  color: "#6773e7",
+                },
+              ]),
+            },
+          },
         },
       ],
     };
@@ -59,12 +96,13 @@ export const Chart2 = () => {
 
   return (
     <div className="bordered 破获排名">
-      <h2>案件破获排名</h2>
-      <div ref={divRef} className="chart"></div>
+      {" "}
+      <h2>案件破获排名</h2> <div ref={divRef} className="chart"></div>{" "}
       <div className="legend">
-        <span className="first">破案排名1</span>
-        <span className="second">破案排名2</span>
-      </div>
+        {" "}
+        <span className="first"></span> 破案排名1{" "}
+        <span className="second"></span> 破案排名2{" "}
+      </div>{" "}
     </div>
   );
 };
