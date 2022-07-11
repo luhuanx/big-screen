2d186496810aca24859b033aed176ea1e0e60292
Mimanchi <1640247539@qq.com>
2022-07-11 11:07:14 +0800

    添加footer

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,28 +1,34 @@
 import React from "react";
 import "./home.scss";
 import headerBg from "../images/header.png";
+
 import { Chart1 } from "../components/chart-1";
+
 import { Chart2 } from "../components/chart-2";
 
 export const Home = () => {
+  const year = new Date().getFullYear();
   return (
     <div className="home">
+      {" "}
       <header
         className="x"
         style={{
           backgroundImage: `url(${headerBg})`,
         }}
-      />
+      />{" "}
       <main>
+        {" "}
         <section className="section1">
-          <Chart1 />
-          <Chart2 />
-        </section>
-        <section className="bordered section2"></section>
-        <section className="bordered section3"></section>
-        <section className="bordered section4"></section>
-        <section className="bordered section5"></section>
-      </main>
+          {" "}
+          <Chart1 /> <Chart2 />{" "}
+        </section>{" "}
+        <section className="bordered section2"></section>{" "}
+        <section className="bordered section3"></section>{" "}
+        <section className="bordered section4"></section>{" "}
+        <section className="bordered section5"></section>{" "}
+      </main>{" "}
+      <footer>&copy; huanxlu 2022-{year}</footer>
     </div>
   );
 };
