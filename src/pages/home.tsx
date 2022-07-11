e520e4664da74b632b6a3e02abd32a2fa855f7b1
Mimanchi <1640247539@qq.com>
2022-07-11 15:06:44 +0800

    制作chart3

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,7 @@ import headerBg from "../images/header.png";
 import { Chart1 } from "../components/chart-1";
 
 import { Chart2 } from "../components/chart-2";
+import { Chart3 } from "../components/chart-3";
 
 export const Home = () => {
   const year = new Date().getFullYear();
@@ -23,7 +24,9 @@ export const Home = () => {
           {" "}
           <Chart1 /> <Chart2 />{" "}
         </section>{" "}
-        <section className="bordered section2"></section>{" "}
+        <section className="section2">
+          <Chart3 />
+        </section>{" "}
         <section className="bordered section3"></section>{" "}
         <section className="bordered section4"></section>{" "}
         <section className="bordered section5"></section>{" "}
