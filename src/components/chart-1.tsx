c9cdaf2939ce8b26dde8497a774fe1c58c9b9a29
Mimanchi <1640247539@qq.com>
2022-07-12 11:14:58 +0800

    完成chart1数据变化

--- src/components/chart-1.tsx
+++ src/components/chart-1.tsx
@@ -1,71 +1,109 @@
 import React, { useEffect, useRef } from "react";
 import * as echarts from "echarts";
+import { createEchartOptions } from "../shared/create-echarts-options";
 
 const px = (n) => (n / 2420) * (window as any).pageWidth;
 
 export const Chart1 = () => {
   const divRef = useRef(null);
+  const myChart = useRef(null);
+  const data = [
+    { name: "兰州新区", 2021: 33 },
+    { name: "兰州新区", 2021: 23 },
+    { name: "兰州新区", 2021: 19 },
+    { name: "兰州新区", 2021: 45 },
+    { name: "兰州新区", 2021: 17 },
+    { name: "兰州新区", 2021: 29 },
+  ];
+
   useEffect(() => {
-    var myChart = echarts.init(divRef.current);
-    var option = {
-      textStyle: {
-        fontSize: px(12),
-        color: "#546dd1",
-      },
-      xAxis: {
-        axisLine: {
-          lineStyle: { color: "#083b70" },
+    setInterval(() => {
+      const newData = [
+        {
+          name: "兰州新区",
+          2021: Math.random() * 50,
         },
-        axisTick: { show: false },
-        data: [
-          "兰州新区",
-          "兰州新区",
-          "兰州新区",
-          "兰州新区",
-          "兰州新区",
-          "兰州新区",
-          "兰州新区",
-          "兰州新区",
-          "兰州新区",
-        ],
-        axisLabel: {
+        {
+          name: "兰州新区",
+          2021: Math.random() * 50,
+        },
+        {
+          name: "兰州新区",
+          2021: Math.random() * 50,
+        },
+        {
+          name: "兰州新区",
+          2021: Math.random() * 50,
+        },
+        {
+          name: "兰州新区",
+          2021: Math.random() * 50,
+        },
+        {
+          name: "兰州新区",
+          2021: Math.random() * 50,
+        },
+      ];
+      x(newData);
+    }, 3000);
+  }, []);
+
+  const x = (data) => {
+    myChart.current.setOption(
+      createEchartOptions({
+        textStyle: {
           fontSize: px(12),
-          formatter(val) {
-            if (val.length > 2) {
-              const arr = val.split("");
-              arr.splice(2, 0, "\n");
-              return arr.join("");
-            } else {
-              return val;
-            }
+          color: "#546dd1",
+        },
+        xAxis: {
+          axisLine: {
+            lineStyle: { color: "#083b70" },
+          },
+          axisTick: { show: false },
+          data: data.map((i) => i.name),
+          axisLabel: {
+            fontSize: px(12),
+            formatter(val) {
+              if (val.length > 2) {
+                const arr = val.split("");
+                arr.splice(2, 0, "\n");
+                return arr.join("");
+              } else {
+                return val;
+              }
+            },
           },
         },
-      },
-      grid: {
-        x: px(40),
-        y: px(30),
-        x2: px(40),
-        y2: px(50),
-      },
-      yAxis: {
-        axisLine: {
-          lineStyle: { color: "#083b70" },
+        grid: {
+          x: px(40),
+          y: px(30),
+          x2: px(40),
+          y2: px(50),
         },
-        splitLine: { show: false },
-        axisLabel: {
-          fontSize: px(12),
+        yAxis: {
+          axisLine: {
+            lineStyle: { color: "#083b70" },
+          },
+          splitLine: { show: false },
+          axisLabel: {
+            fontSize: px(12),
+          },
         },
-      },
-      series: [
-        {
-          name: "销量",
-          type: "bar",
-          data: [10, 20, 36, 41, 15, 26, 37, 18, 29],
-        },
-      ],
-    };
-    myChart.setOption(option);
+        series: [
+          {
+            type: "bar",
+            data: data.map((i) => i[2021]),
+          },
+        ],
+      })
+    );
+  };
+
+  useEffect(() => {
+    myChart.current = echarts.init(divRef.current);
+    x(data);
   }, []);
+
   return (
     <div className="bordered 管辖统计">
       <h2>案发派出所管辖统计</h2>
